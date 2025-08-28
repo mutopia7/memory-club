1480b0c2be2c62d16d6f1b6af578d29e461abf52
Mohammadrasool Vahid <121235436+mutopia7@users.noreply.github.com>
2025-08-28 23:41:32 +0330

    chore(data): set pool and linked to the database

---
+++ db/pool.js
@@ -0,0 +1,21 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+let pool;
+
+if (process.env.DATABASE_URL) {
+    pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }
+    });
+} else {
+    pool = new Pool({
+        host: process.env.HOST,
+        user: process.env.USER,
+        database: process.env.DATABASE,
+        port: process.env.DB_PORT
+    });
+}
+
+module.exports = pool;
+
