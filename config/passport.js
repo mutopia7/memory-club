1b11b88deee22045a7b0c69b8cb55ebfdc9bd941
Mohammadrasool Vahid <121235436+mutopia7@users.noreply.github.com>
2025-08-31 16:51:31 +0330

    feat(logic): improve security with bcrypt

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const pool = require("../db/pool")
+const bcrypt = require("bcryptjs")
 
 passport.use(
   new LocalStrategy(async (username, password, done) => {
@@ -11,7 +12,9 @@ passport.use(
       if (!user) {
         return done(null, false, { message: "Incorrect username" });
       }
-      if (user.password !== password) {
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
         return done(null, false, { message: "Incorrect password" });
       }
       return done(null, user);
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,11 @@
 const db = require("../db/queries")
+const bcrypt = require("bcryptjs")
 
 async function signUpPost(req, res, next) {
     try {
     const { firstname, lastname, username, password } = req.body;
-    await db.createUser(firstname, lastname,username, password)
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await db.createUser(firstname, lastname,username, hashedPassword)
     res.redirect("/")
     } catch (err) {
         return next(err)
