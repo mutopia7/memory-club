f641cc7ecb71dfa1cdb61d91fc32548da28b0841
Mohammadrasool Vahid <121235436+mutopia7@users.noreply.github.com>
2025-09-04 17:37:51 +0330

    feat(style) : style ejs files and create header and footer

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, "public")))
 
 app.use("/", router)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Listening on Port ${PORT}`)
